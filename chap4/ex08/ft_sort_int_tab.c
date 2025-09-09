6c69e84d591e6a011b2c33c65e822016f65bb63f
Julie Tardieu <jtardieu@k1r4p10.42mulhouse.fr>
2025-09-09 16:59:20 +0200

    alors j ai pitete kekra

--- chap4/ex08/ft_sort_int_tab.c
+++ chap4/ex08/ft_sort_int_tab.c
@@ -1,47 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jtardieu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/08 17:25:45 by jtardieu          #+#    #+#             */
-/*   Updated: 2025/09/08 17:25:50 by jtardieu         ###   ########.fr       */
+/*   Created: 2025/09/09 15:46:50 by jtardieu          #+#    #+#             */
+/*   Updated: 2025/09/09 15:46:53 by jtardieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
-void	ft_rev_int_tab(int *tab, int size);
+void	ft_sort_int_tab(int *tab, int size);
 void	ft_swap(int *a, int *b);
 
 // int main()
 // {
-// 	int taille = 10;
-// 	int tab[taille];
-// 	unsigned int i =0; 
-// 	while (i!=taille+1)
-// 	{
-// 		tab[i]=i;
-// 		i++;
-// 	}
-// 	ft_rev_int_tab(tab,taille);
-// 	int j =0;
+// 	int taille = 15;
+// 	//int tab [] = {456,1,542,23,547,865,45,25,641,2,8,3,54,21,6};
+// 	int tab[]= {5,14,12,3,9,10,7,1,15,2,11,13,4,6,8};
+// 	ft_sort_int_tab(tab, taille);
+
+// 	int j=0;
 // 	while (j<=taille)
 // 	{
 // 		printf("num %d = %d \n",j,tab[j]);
 // 		j++;
 // 	}
 // }
-void	ft_rev_int_tab(int *tab, int size)
+void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
-	int	tamp;
 
-	i = 0;
-	while (i <= size / 2)
+	i = -1;
+	while (i++ < size -1)
 	{
-		ft_swap(&tab[i], &tab[size - i]);
-		i++;
+		if (tab[i] > tab[i + 1])
+		{
+			ft_swap(&tab[i], &tab[i + 1]);
+			i = -1;
+		}
 	}
 }
 
