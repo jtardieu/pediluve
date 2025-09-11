e4e56676cb4ae76634fef6bb420349afc8de95cd
Julie Tardieu <jtardieu@k1r4p9.42mulhouse.fr>
2025-09-11 19:35:21 +0200

    ta grande darone de sa < la normimerde de con

--- chap5/ex01/ft_strncpy.c
+++ chap5/ex01/ft_strncpy.c
@@ -27,13 +27,14 @@ int main ()
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
+
 	i = 0;
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (i < n )
+	while (i > n)
 	{
 		dest[i] = 0;
 		i++;
